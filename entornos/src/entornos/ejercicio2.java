239eb8cab8a93a6a92fedc0587146b86a0d6d173
Ismael m <Ismael m@DESKTOP-SFIIMMD.localdomain>
2020-11-04 09:46:59 +0100

    ejercicio 2

---
+++ entornos/src/entornos/ejercicio2.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package entornos;
+
+/**
+ * @author Ismael m
+ *
+ */
+public class ejercicio2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("adios mundo");
+		//he puesto adios mundo
+
+	}
+
+}
